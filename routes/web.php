3bd736d523f2b9bfa951e9712d50242975a1e8fa
Syofyan Zuhad 🇵🇸 <sofyanzuhad2@gmail.com>
2024-10-09 19:44:53 +0700

    update: route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ok', function () {
+    return 'OK';
+});
+
+Route::get('/json', function () {
+    return return response()->json([
+        'message' => 'Hello World',
+    ]);
+});
